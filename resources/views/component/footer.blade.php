3ff1ca12bf329990eea5105641da013163a5cee8
apikammar <apikammar07@gmail.com>
2023-12-02 01:55:04 +0800

    component setup

---
+++ resources/views/component/footer.blade.php
@@ -0,0 +1,5 @@
+    <footer class="bg-dark text-white fixed-bottom text-center py-2">
+        <div class="container">
+            <p class="h6">&copy; {{ date('Y') }} By Afiq Ammar. All rights reserved.</p>
+        </div>
+    </footer>
---
+++ resources/views/testpage.blade.php
@@ -0,0 +1,7 @@
+@extends('component/master')
+
+@section('title', 'firstpagetest')
+    
+@section('content')
+    <button class="btn btn-lg btn-primary">TESTING</button>    
+@endsection
